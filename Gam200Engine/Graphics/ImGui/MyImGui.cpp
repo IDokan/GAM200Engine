1761b7351e0b25021d8c974d2328ff8e42059971
Sinil <rtd99062@gmail.com>
2019-08-24 19:31:56 +0900

    Now docking is available!
    Next task is make enable multiple viewport!

--- Gam200Engine/Graphics/ImGui/MyImGui.cpp
+++ Gam200Engine/Graphics/ImGui/MyImGui.cpp
@@ -13,6 +13,7 @@ Creation Date: 08.23.2019
 #include <Graphics/ImGui/imgui.h>
 #include <Graphics/ImGui/imgui_impl_opengl3.h>
 #include <Graphics/ImGui/imgui_impl_glfw.h>
+#include <Application.hpp>
 
 namespace MyImGui
 {
@@ -24,18 +25,53 @@ namespace MyImGui
 		ImGui_ImplOpenGL3_Init("#version 330");
 	}
 
+	// Merge at one or make it separate kind of Begin, Update, End...
 	void UpdateImGui(bool isShowWindow) noexcept
 	{
+		// Old & Easy version to only use pop up the ImGui window
+		//ImGui_ImplOpenGL3_NewFrame();
+		//ImGui_ImplGlfw_NewFrame();
+		//ImGui::NewFrame();
+
+		//if (isShowWindow)
+		//	ImGui::ShowDemoWindow(&isShowWindow);
+
+		//// Rendering
+		//ImGui::Render();
+		//ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+	// Our state
+		static bool show_demo_window = true;
+		static bool show_another_window = false;
+		static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+				// Start the Dear ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		if (isShowWindow)
-			ImGui::ShowDemoWindow(&isShowWindow);
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
 
+
+		ImGuiIO& io = ImGui::GetIO();
+		auto* app = Application::GetApplication();
+		io.DisplaySize = ImVec2(app->GetWindowSize.x, app->GetWindowSize.y);
 		// Rendering
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Update and Render additional Platform Windows
+		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+		//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
 	}
 
 	void ClearImGui() noexcept
