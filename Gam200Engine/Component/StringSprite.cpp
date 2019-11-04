7b6a8beb5669cea2741a1963dfa9c900ae6bf7b2
Il <rtd990@naver.com>
2019-11-04 15:09:26 +0900

    No big change, a few of maintenance.
    Sprite.hpp - include Component header to show declarations because Sprite is a derived class of Component class.
    StringSprite.cpp&hpp - Start to implement string optimized version of sprite.
    Gam200Engine.vcxproj & .filters - relocate a few of files (physics, string_sprite).
    texture/rope.png - image file for string.
    
    CIB - Robin

---
+++ Gam200Engine/Component/StringSprite.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+Copyright (C) 2019 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name:   StringSprite.cpp
+Author
+	- Sinil.Kang rtd99062@gmail.com
+Creation Date: 11.04.2019
+
+	Header file for Sprite implementation for string graphics
+******************************************************************************/
+#include <Component/StringSprite.hpp>
+
+StringSprite::StringSprite(Object* obj) noexcept
+	: Sprite(obj)
+{
+}
+
+void StringSprite::Init()
+{
+	Sprite::Init();
+
+	SetImage("../texture/rope.png");
+}
+
+void StringSprite::Update(float dt)
+{
+	Sprite::Update(dt);
+}
+
+void StringSprite::Clear()
+{
+	Sprite::Clear();
+}
