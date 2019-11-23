aefda40afb42ddda4f5701c503d9073704cde925
hyerin jung <junghl0621@gmail.com>
2019-11-23 14:44:50 +0900

    for FileIO
    with jw

---
+++ Gam200Engine/FileIO.cpp
@@ -0,0 +1,11 @@
+/******************************************************************************
+Copyright (C) 2019 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name:   Application.cpp
+Author
+    - jaemin.woo wjm9932@gmail.com
+Creation Date: 08.05.2019
+
+    Source file for the make window
+******************************************************************************/
---
+++ Gam200Engine/FileIO.hpp
@@ -0,0 +1,26 @@
+#pragma once
+/******************************************************************************
+Copyright (C) 2019 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name:   Application.cpp
+Author
+    - jaemin.woo wjm9932@gmail.com
+Creation Date: 08.05.2019
+
+    Source file for the make window
+******************************************************************************/
+
+struct ObjectData {
+    int xpos;
+    int ypos;
+    int size;
+    int transform;
+    int speed;
+};
+
+class fileIO {
+    fileIO();
+
+                                                                            
+};
