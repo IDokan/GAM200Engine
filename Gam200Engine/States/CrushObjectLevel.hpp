c88e8ee40a82a5ea32462f19b69800dff1e72772
yoonsoo <dbsqhd106@gmail.com>
2019-12-13 04:19:05 +0900

    Implement CrushObjectLevel
    CIB-JAMJAM

---
+++ Gam200Engine/States/CrushObjectLevel.hpp
@@ -0,0 +1,57 @@
+/******************************************************************************
+Copyright (C) 2019 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name: CrushObjectLevel.hpp
+Author: dbsqhd106@gmail.com
+
+Creation Date: DEC/11st/2019
+
+    Header file for the test object whether interact work or not
+******************************************************************************/
+
+#pragma once
+#include <States/State.hpp>
+#include <Object/InteractiveObject/CrushableObject.hpp>
+
+class Object;
+class String;
+
+class CrushObjectLevel : public State
+{
+public:
+    CrushObjectLevel();
+    void UpdateCollisionBox(Object* obj1, Object* obj2);
+    virtual ~CrushObjectLevel();
+
+    void Update(float dt) override;
+
+protected:
+    void GameRestart() override;
+    void Load() override;
+    void Unload() override;
+
+    void Input();
+    void Collision();
+    void InitObject();
+private:
+    Object* background;
+    Object* startPoint{};
+    Object* goalPoint{};
+
+    Object* first_Objects_1{};
+    Object* first_Objects_2{};
+    Object* first_Objects_3{};
+
+    Object* player1{};
+    Object* player2{};
+
+    CrushableObject* object1;
+    CrushableObject* object2;
+    CrushableObject* object3;
+    CrushableObject* object4;
+    CrushableObject* object5;
+    CrushableObject* object6;
+
+    String* string{};
+};
